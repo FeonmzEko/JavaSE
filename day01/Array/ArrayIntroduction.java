2ee68b2027c13ef1626629b924e7be134d339487
dahiajw <3460213800@qq.com>
2025-08-14 09:29:28 +0800

    second

--- day01/Array/ArrayIntroduction.java
+++ day01/Array/ArrayIntroduction.java
@@ -1,12 +1,14 @@
 package Array;
 
+import java.util.Arrays;
+
 public class ArrayIntroduction {
     public static void main(String[] args) {
         int[] array = {1,2,3};
         String[] array1 = {"zhangsan","lisi"};
         double[] array2 = {1.1,1.3,1.5};
         System.out.println(array[0]);
-        System.out.println(array1);
-        System.out.println(array2);
+        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(array2));
     }
 }
